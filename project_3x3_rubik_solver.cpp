a7061397588d91cd4b75b7550c7acd36004e2238
Hassan Khalloof <2024006840@aurak.ac.ae>
2024-11-21 16:54:04 +0400

    Add Visual Studio files

---
+++ project_3x3_rubik_solver.cpp
@@ -0,0 +1,12 @@
+﻿// project_3x3_rubik_solver1.cpp : Defines the entry point for the application.
+//
+
+#include "project_3x3_rubik_solver1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
