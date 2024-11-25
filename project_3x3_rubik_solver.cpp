756695426a0754de7f12822ca9324a12ec9b25cb
22003 <hasnainlol69@gmail.com>
2024-11-25 09:40:57 +0400

    F2l Algorithm function need to add more algorithms

--- project_3x3_rubik_solver.cpp
+++ project_3x3_rubik_solver.cpp
@@ -66,6 +66,7 @@ void insertF2lPair(int edgeFace, int cornerFace);
 void performF2LAlgorithm1();
 void performF2LAlgorithm2();
 void performF2LAlgorithm3();
+void performF2LAlgorithm4();
 
 
 void scramble();
@@ -533,29 +534,33 @@ bool isPairAligned(int edgeFace, int cornerFace) {
 
 
 void orientPair(int edgeFace, int cornerFace) {
-	if (cube[edgeFace][0][1] == 'W') {
-		// Edge is flipped
-		moveU();
-		performF2LAlgorithm1();
+	// Identify the orientation and relative position of the edge and corner
+	// Case 1: Edge on top, corner directly above
+	if (cube[edgeFace][0][1] == 'W' && cube[cornerFace][0][0] == 'W') {
+		performF2LAlgorithm1(); // U R U' R' U' F' U F
 	}
-	else if (cube[cornerFace][0][0] == 'W') {
-		// Corner needs rotation
-		moveUPrime();
-		performF2LAlgorithm2();
+	// Case 2: Edge in front right, corner in top right
+	else if (cube[edgeFace][1][2] == 'W' && cube[cornerFace][0][2] == 'W') {
+		performF2LAlgorithm2(); // U R U' R'
 	}
-	else {
-		// General misalignment case
-		performF2LAlgorithm3();
+	// Case 3: Edge in back right, corner in top right
+	else if (cube[edgeFace][2][2] == 'W' && cube[cornerFace][0][2] == 'W') {
+		performF2LAlgorithm3(); // U' L' U L
 	}
-	// Step 2: If the edge-corner pair is misaligned (in a different orientation), apply another algorithm
+	// Case 4: Edge in front left, corner in top left
+	else if (cube[edgeFace][0][0] == 'W' && cube[cornerFace][0][0] == 'W') {
+		performF2LAlgorithm4(); // U' L' U L U F U' F'
+	}
+	// Continue with the remaining cases...
+	// Each case should perform a different algorithm as defined
+
+	// Edge in the top layer but misaligned
 	if (cube[edgeFace][0][2] == 'W' && cube[cornerFace][2][0] == 'W') {
 		performF2LAlgorithm2();
 	}
-	// Step 3: Apply an algorithm to handle all other cases
-	else {
-		performF2LAlgorithm3();
-	}
+	// Apply other algorithms
 }
+
 
 void insertF2lPair(int edgeFace, int cornerFace) {
 	// Insert using the appropriate sequence based on the position
