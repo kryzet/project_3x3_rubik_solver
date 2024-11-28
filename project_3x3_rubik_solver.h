86be76026018f40efb96db270b33679f9d84be1b
Hassan Khalloof <166802607+kryzet@users.noreply.github.com>
2024-11-28 17:03:38 +0400

    Starting work on Kociemba's algorithm

--- project_3x3_rubik_solver.h
+++ project_3x3_rubik_solver.h
@@ -7,6 +7,7 @@
 
 // TODO: Reference additional headers your program requires here.
 #include <array>
-#include <string>
-#include <cstdbool>
-//#include <limits>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
